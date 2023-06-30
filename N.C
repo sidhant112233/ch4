7853523b9240bd589f9322b57c78102b52faabb6
sidhant112233 <137783068+sidhant112233@users.noreply.github.com>
2023-06-30 03:40:42 -0700

    Add files via upload

---
+++ N.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+       clrscr();
+       int a,b,c;
+       a=5,b=5;
+      c=(a*a*a)-(a*a*a)+(b*b*b)+((3*a*b)*(a+b));
+       printf("(a+b)3=%d\n",c);
+       getch();
+}
---
+++ SUB.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+       clrscr();
+       int a,b,c;
+       a = 11;
+       b = 11;
+       c = a*b;
+
+       printf("maltiple of a*c=%d",c);
+
+
+
+       getch();
+}
+
